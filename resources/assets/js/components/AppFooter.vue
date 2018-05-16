380545d384ca7eba7c138d6066c39c8d95dfea91
diplomiani <liverliver34@gmail.com>
2018-05-16 15:44:48 +0400

    Vuetify , VueRouter and initial Components

---
+++ resources/assets/js/components/AppFooter.vue
@@ -0,0 +1,14 @@
+<template>
+  <v-footer class="pa-3">
+  	Scouser03
+    <v-spacer></v-spacer>
+    <div>&copy; {{ new Date().getFullYear() }}</div>
+  </v-footer>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
